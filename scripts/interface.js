c08eb80f442f964c4e5d111e657ba131ee759822
Thiago Yudi <thiago.yamada312@gmail.com>
2022-07-21 18:10:51 -0300

    commit

---
+++ scripts/interface.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+})
+
+function handleClick(event) {
+
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+            alert("O jogo acabou - O vencedor foi " + playerTime);
+        }, 10);
+
+    };
+    updateSquare(position);
+
+}
+
+function updateSquare(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`;
+}
